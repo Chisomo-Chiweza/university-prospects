81520bcf793fac7a3b3ee6fc3eecca8e2277421d
ALEX-BANDA <bed-com-07-17@unima.ac.mw>
2022-01-31 16:12:48 +0200

    Added some simple layout box border

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 function App() {
   return (
-    <div className="bg-space">
-      <h1 className="text-4xl text-jonquil">University Prospects</h1>
+    <div class="box-border h-25 w-35 p-12 border-4">
+    <div className="bg-space flex flex-col p-10 box-sizing: border-box ">
+      <h1 className="text-ivory text-7xl font-bold place-self-center">University Prospectus</h1>
+      <button className="text-space bg-white place-self-center p-2 px-6 border-2 
+        rounded-md font-semibold m-8 mt-20 focus:ring-4 focus:ring-ivory 
+        hover:delay-100 hover:bg-space hover:text-ivory shadow-2xl">
+        Get started
+      </button>
+    
+    </div>
     </div>
   );
-}
+ }
 
 export default App;
