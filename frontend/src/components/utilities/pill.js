45e84df44b3530acc3b7dd04c7cebafbff8fd31b
Chisomo Chiweza <chisomchiwezapersonal@gmail.com>
2022-02-01 14:04:59 +0200

    Created fifth utility component: Pill button

---
+++ frontend/src/components/utilities/pill.js
@@ -0,0 +1,15 @@
+import Arrow from "../assets/right-arrow-light.png";
+
+function Pill() {
+    
+    return(
+
+        <button className="justify-center p-1 px-6 mx-auto mt-10 bg-space border-2 border-white rounded-3xl text-xl font-bold focus:ring-2 focus:ring-blue-500">
+            <img src={Arrow} alt="right arrow"></img>
+        </button>
+         
+    );
+
+}
+
+export default Pill;
