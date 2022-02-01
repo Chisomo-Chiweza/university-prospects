d3af3cc9eec02cea77adcaf0c9907946c30ec370
Chisomo Chiweza <chisomchiwezapersonal@gmail.com>
2022-02-01 09:23:43 +0200

    Created first utility component : homepage

---
+++ frontend/src/components/utilities/homepage.js
@@ -0,0 +1,18 @@
+import logo from "../assets/logo.png";
+
+function Homepage(props) {
+
+    return(
+        <div className="h-screen w-screen flex flex-col bg-space">
+
+            <div className="flex flex-row mt-10 justify-center">
+                <img src={logo} alt="Logo" className="object-scale-down object-top"></img>
+            </div>
+
+            <div className="flex flex-row justify-center">{props.content}</div>
+
+        </div>
+    );
+}
+
+export default Homepage;
