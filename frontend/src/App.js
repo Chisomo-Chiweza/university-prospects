de07f91b521a62736972dbcefd684471b48c9cc2
ALEX-BANDA <bed-com-07-17@unima.ac.mw>
2022-02-01 12:38:11 +0200

    addition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 function App() {
   return (
-    <div class="box-border h-25 w-35 p-12 border-4">
-    <div className="bg-space flex flex-col p-10 box-sizing: border-box ">
-      <h1 className="text-ivory text-7xl font-bold place-self-center">University Prospectus</h1>
-      <button className="text-space bg-white place-self-center p-2 px-6 border-2 
+    <div class="box-border h-15 w-35 p-2 border-4 bg-ivory bg-auto">
+    <div className="bg-space p-10 box-sizing: border-box rounded-xl 
+      shadow-lg  space-x-4 ">
+      <h1 className="text-ivory text-8xl font-bold place-self-center">University Prospectus</h1>
+      <button className="text-space bg-white place-self-center p-4 px-6 border-2 
         rounded-md font-semibold m-8 mt-20 focus:ring-4 focus:ring-ivory 
         hover:delay-100 hover:bg-space hover:text-ivory shadow-2xl">
         Get started
