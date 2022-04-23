ff0b674ad452c0a627c6e94f09b1d2e0e5d29774
Chisomo Chiweza <chisomochiwezapersonal@gmail>
2022-04-23 13:33:30 +0200

    Refactored KUHES functions to take both IGCSE and MSCE options

--- frontend/src/components/generate/GenerateProgrammes.js
+++ frontend/src/components/generate/GenerateProgrammes.js
@@ -1,5 +1,5 @@
-// import generateKUHES from './KUHES';
-import generateLUANAR from './LUANAR';
+import generateKUHES from './KUHES';
+// import generateLUANAR from './LUANAR';
 // import generateMUBAS from './MUBAS';
 // import generateMUST from './MUST';
 // import generateUNIMA from './UNIMA';
@@ -10,18 +10,18 @@ export function generateProgrammes(subjects, programmes) {
     const recommendedProgrammes = [];
     const IGCSE = ['A+', 'A', 'B', 'C',];
     
-    // let temp = generateKUHES(subjects, programmes, IGCSE);
-    // if (temp.length > 0) {
+    let temp = generateKUHES(subjects, programmes, IGCSE);
+    if (temp.length > 0) {
 
-    //     for (let i = 0; i < temp.length; i++) {
-    //         const programme = temp[i];
-    //         recommendedProgrammes.push(programme);
-    //     }
-    //     while (temp.length > 0) {
-    //         temp.pop();
-    //     }
+        for (let i = 0; i < temp.length; i++) {
+            const programme = temp[i];
+            recommendedProgrammes.push(programme);
+        }
+        while (temp.length > 0) {
+            temp.pop();
+        }
 
-    // }
+    }
 
     // temp = generateMUBAS(subjects, programmes, IGCSE);
     // if (temp.length > 0) {
@@ -49,18 +49,18 @@ export function generateProgrammes(subjects, programmes) {
 
     // }
     
-    let temp = generateLUANAR(subjects, programmes, IGCSE);
-    if (temp.length > 0) {
+    // temp = generateLUANAR(subjects, programmes, IGCSE);
+    // if (temp.length > 0) {
 
-        for (let i = 0; i < temp.length; i++) {
-            const programme = temp[i];
-            recommendedProgrammes.push(programme);
-        }
-        while (temp.length > 0) {
-            temp.pop();
-        }
+    //     for (let i = 0; i < temp.length; i++) {
+    //         const programme = temp[i];
+    //         recommendedProgrammes.push(programme);
+    //     }
+    //     while (temp.length > 0) {
+    //         temp.pop();
+    //     }
 
-    }
+    // }
 
     // temp = generateUNIMA(subjects, programmes, IGCSE);
     // if (temp.length > 0) {
